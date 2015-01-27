72516a6445e1cfc428dfb6216fbd23ca950fe850
Lauren Pittenger <lepittenger@gmail.com>
2015-01-27 17:33:54 -0500

    add function to add class to linked images for styling

--- functions.php
+++ functions.php
@@ -15,4 +15,22 @@ function my_scripts_method() {
 
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 
+/**
+ * Attach a class to linked images' parent anchors
+ * e.g. a img => a.img img
+ */
+function give_linked_images_class($html, $id, $caption, $title, $align, $url, $size, $alt = '' ){
+  $classes = 'img'; // separated by spaces, e.g. 'img image-link'
+
+  // check if there are already classes assigned to the anchor
+  if (preg_match('/(<a.*?)(class=".*">)/', $html)) {
+	$html = preg_replace('/(<a.*? class=")(.*?" )(.*?\="" ?="">)/', '$1 ' . $classes . ' $2 $3', $html);
+} else {
+	$html = preg_replace('/(<a.*?)(.*?>)/', '$1 class="' . $classes . '" $2', $html);
+}
+  return $html;
+}
+add_filter('image_send_to_editor','give_linked_images_class',10,8);
+
+
 ?>
